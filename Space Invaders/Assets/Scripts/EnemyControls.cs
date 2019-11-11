e4d8c5a30a8ee1caf8fab1ed14888f8d74ed9139
Chewy <ashleyycheemaa@hotmail.com>
2019-11-11 11:08:30 +0000

    Added enemycontrols and gyroscope

---
+++ Space Invaders/Assets/Scripts/EnemyControls.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControls : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float speed;
+    private float fireTime;
+    private float nextFire;
+
+    private float leftSideScreen = 0.2f;
+    private float rightSideScren = 0.8f;
+
+    private bool touchSide;
+    private Vector2 screenBounds;
+
+    public GameObject instaPrefab;
+    public Transform spawnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyBounds();
+
+        if(Time.time > nextFire)
+        {
+            Fire();
+            nextFire = Time.time + fireTime;
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject rocket = Instantiate(instaPrefab, spawnPos.transform.position, spawnPos.transform.rotation) as GameObject;
+    }
+
+    private void EnemyBounds()
+    {
+        screenBounds.x = Mathf.Clamp(screenBounds.x, leftSideScreen, rightSideScren);
+        transform.position = Camera.main.ViewportToWorldPoint(screenBounds);
+
+        if(transform.position.y <= -screenBounds.y)
+        {
+            Destroy(gameObject);
+        }
+
+        if(screenBounds.x >= rightSideScren)
+        {
+            touchSide = true;
+        }
+        else if(screenBounds.x <= leftSideScreen)
+        {
+            touchSide = false;
+        }
+
+        Movement(speed);
+
+    }
+
+    private void Movement(float direction)
+    {
+        if(!touchSide)
+        {
+            rb.velocity = transform.right * direction;
+        }
+        else
+        {
+            rb.velocity = transform.right * -direction;
+        }
+    }
+
+    private void DownMovement()
+    {
+        Vector2 position = transform.position;
+        position.y -= Time.deltaTime;
+        transform.position = position;
+    }
+}
+
---
+++ Space Invaders/Assets/Scripts/Gyroscope.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gyroscope : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private UnityEngine.Gyroscope gyro;
+
+    private GameObject cameraContain;
+    private Quaternion rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraContain = new GameObject("Camera Container");
+        cameraContain.transform.position = transform.position;
+        transform.SetParent(cameraContain.transform);
+
+        gyroEnabled = EnableGyro();
+    }
+
+    private void Update()
+    {
+        if(gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rotation;
+        }
+    }
+
+    private bool EnableGyro()
+    {
+        if(SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            cameraContain.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rotation = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+
+        return false;
+    }
+}
